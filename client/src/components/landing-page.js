512c72d34db7eba595e00d0f8e99a03b1f57ad22
Ngoc Tina Hoang <ngoc.tina.hoang@outlook.com>
2017-12-17 20:55:21 -0500

    styling

--- client/src/components/landing-page.js
+++ client/src/components/landing-page.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {connect} from 'react-redux';
 import {Link, Redirect} from 'react-router-dom';
-import './landing-page';
+import './landing-page.css';
 import LoginForm from './login-form';
 import Banner from './banner.png';
 
