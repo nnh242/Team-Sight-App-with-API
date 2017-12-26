b1815f146db93b6b209d894719edcda92b50219d
Ngoc Tina Hoang <ngoc.tina.hoang@outlook.com>
2017-12-26 01:30:33 -0500

    fixed img on front page

--- client/src/components/landing-page.js
+++ client/src/components/landing-page.js
@@ -4,7 +4,7 @@ import {Link, Redirect} from 'react-router-dom';
 import './landing-page.css';
 import LoginForm from './login-form';
 import Banner from './banner.png';
-
+import TeamScreen from './team.png';
 export function LandingPage(props) {
     if (props.loggedIn) {
         return <Redirect to="/dashboard" />;
@@ -16,14 +16,13 @@ export function LandingPage(props) {
                 <header>
                     <h3>Manage your team members</h3>
                 </header>
-                <p>[<em>placeholder for team's team</em>]</p>
                 <p>TeamSight helps you become aware of what your team members are working on, compare the estimated and actual duration of their tasks. You can use this app to manage tasks and members, and use simple metrics to predict project's duration. </p>
             </section>
             <section>
                 <header>
                     <h3>Assign tasks and members</h3>
                 </header>
-                <p>[<em>placeholder for screenshot of taskteams</em>]</p>
+                <img src={TeamScreen} alt="team" className="team-img"/>
                 <p>The key to manage a good project is to define roles and related tasks with smart goals. A constant struggle for a project manager is to gather the current tasks that members are working on and how long they take to complete the tasks.TeamSight provides you with a quick and simple interface that helps you keep track of what your members are working on and how long it takes them to finish.</p>
             </section>
             <section>
